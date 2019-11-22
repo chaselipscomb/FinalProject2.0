ef1b506481d46acbf191ef777b9cda04aa5d247d
Chase Lipscomb <chaselipscomb97@gmail.com>
2019-11-22 13:08:39 -0800

    start template

---
+++ src/components/pages/SearchHero.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function SearchHero() {
+    return(
+        <React.Fragment>
+            <div>hero</div>
+        </React.Fragment>
+    )
+}
+export default SearchHero;
